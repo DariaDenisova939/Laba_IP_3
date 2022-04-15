a19eb5a80a00f125415e65f985d25a91c46ad6a4
daria <denisovadara939@gmail.com>
2022-04-15 21:09:54 +0300

    commit

--- index.php
+++ index.php
@@ -6,30 +6,28 @@
     <script type="text/javascript" src="labaIP/scripts/jquery-3.6.0.js"></script>
     <link rel="stylesheet" href="labaIP/styles/style.css">
     <script type="text/javascript" src="labaIP/scripts/scripts_lab_3.js"></script>
-
-
 </head>
 <body>
-
 <div class="main_content">
     <div class="block_page_one">
         <div>
             <h1><center>Website about me</center></h1>
         </div>
-
     </div>
     <div class="text about me">
         <p align="center">
             My name is Daria Denisova, I am 18 years old. I study at the Technical University at the Faculty of Computer Science and Computer Engineering in the city of Ulyanovsk. I graduated from gymnasium number 59. I have a brother and a dad who support me in all my endeavors. I am fond of programming. From sports hobbies - volleyball. I like to spend time with friends, watch movies and listen to music.
         </p>
-
     </div align="center">
     <p align="center"> My photos </p>
     <div class="main_gallery">
-        <input class="buttonGallery_1" type="button" onclick="leftChangeImg()" name="nubex" value="left" />
         <div id="mainImage">
-
         </div>
+    </div>
+    <div>
+        <input class="buttonGallery_1" type="button" onclick="leftChangeImg()" name="nubex" value="left" />
+    </div>
+    <div>
         <input class="buttonGallery_2" type="button" onclick="rightChangeImg()" name="nubex" value="right" />
     </div>
     <div class="block_about_me_1">
@@ -43,7 +41,6 @@
             <p class="size">3. Silicon Valley</p>
             The story is about a group of geeks preparing to launch their own startups in the high-tech center of San Francisco.
             The main characters of the series live for free in the house of a local millionaire, but in return they will have to give 10% of the profits from future projects.
-
         </details>
     </div>
     <div class="block_about_me_2">
@@ -55,11 +52,10 @@
             American singer, songwriter and poet. Her music has been noted by critics for its cinematic style, concern for tragic relationships and melancholy, as well as for references to American pop culture, in particular, the 1950s and 1960s.
             <p class="size"> 3. Arctic Monkeys</p>
             British rock band formed in 2002 in High Green, a suburb of Sheffield. Currently consists of vocalist and guitarist Alex Turner, guitarist Jamie Cook, drummer Matt Helders and bass guitarist Nick O'Malley, who replaced Andy Nicholson, who previously played in the band, who left the band in 2006, shortly after the release of the debut album.
-
         </details>
         <details>
             <summary class="font">Volleyball is the best sport</summary>
-            During my school volleyball life, I received many awards, participated in all competitions between schools. Volleyball has taught me resilience, endurance and discipline. Even taking into account professional injuries, volleyball is not included in the rating of the most traumatic sports in any way. With regular exercise, it hardens the body, strengthens the immune system, improves the circulatory system and the work of the cardiovascular and respiratory systems. Volleyball has a very positive effect on the nervous system: during the game it works at the limit — it is necessary to monitor the fast-moving players, perform their role, make decisions. In volleyball, you need to be very attentive and react very quickly, both physically and mentally. In professional matches, the speed of the ball can reach 130 kilometers per hour! Volleyball is a team game. In the classic version, the team consists of 6 people. Each of them has its own zones and specific tasks, but success always depends on everyone! A well-received ball, a clear pass to the attacker — and now the opponent is grieving because he didn't even have time to block the ball. The game of volleyball brings you closer, breaks the boundaries: it doesn't matter on the field whether you are a boss or a subordinate, insults and misunderstandings are forgotten, you are united by one task — and this is the main thing! Add to this a sense of social responsibility, hard work, courage and discipline - volleyball not only teaches, it educates
+            <p clas="text"> During my school volleyball life, I received many awards, participated in all competitions between schools. Volleyball has taught me resilience, endurance and discipline. Even taking into account professional injuries, volleyball is not included in the rating of the most traumatic sports in any way. With regular exercise, it hardens the body, strengthens the immune system, improves the circulatory system and the work of the cardiovascular and respiratory systems. Volleyball has a very positive effect on the nervous system: during the game it works at the limit — it is necessary to monitor the fast-moving players, perform their role, make decisions. In volleyball, you need to be very attentive and react very quickly, both physically and mentally. In professional matches, the speed of the ball can reach 130 kilometers per hour! Volleyball is a team game. In the classic version, the team consists of 6 people. Each of them has its own zones and specific tasks, but success always depends on everyone! A well-received ball, a clear pass to the attacker — and now the opponent is grieving because he didn't even have time to block the ball. The game of volleyball brings you closer, breaks the boundaries: it doesn't matter on the field whether you are a boss or a subordinate, insults and misunderstandings are forgotten, you are united by one task — and this is the main thing! Add to this a sense of social responsibility, hard work, courage and discipline - volleyball not only teaches, it educates <p>
         </details>
         <details>
             <summary class="font">My University curriculum</summary>
@@ -70,9 +66,7 @@
             <p class="size"> 3. System-technical cycle</p>
             System analysis, System Modeling, Distributed Systems and Parallel Computing Technologies, Information Security Technologies, Information Systems Design, Computer Design Technologies, Data Mining, Artificial Intelligence Methods and Systems, IT Project Management, Physics, Electronics and Electrical Engineering, Computer Circuitry and Computer Architecture, Computer Networks, Microprocessor Systems.
         </details>
-
     </div>
-
 </div>
 </body>
 </html>
